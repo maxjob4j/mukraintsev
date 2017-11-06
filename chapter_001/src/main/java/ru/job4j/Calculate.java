fcf179024fb7a79a14726c682fe626caf540a657
maxjob4j <mailto:prosto2@bk.ru>
2017-11-06 23:41:09 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j;
 
+/**
+*Class Calculate решение задачи части 001 урок1.
+*@autor ukraintsev
+*@since 06.11.2017
+*/
 public class Calculate {
 	public static void main (String[] args){
 		System.out.print("Hello world");
