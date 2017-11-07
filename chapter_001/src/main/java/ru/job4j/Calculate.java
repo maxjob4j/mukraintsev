756b04ce0fd4e201916496dba7453822fe7dc86a
maxjob4j <mailto:prosto2@bk.ru>
2017-11-07 03:10:08 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -9,4 +9,13 @@ public class Calculate {
 	public static void main (String[] args){
 		System.out.print("Hello world");
 	}
+
+/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+	public String echo(String name) {
+   		return "Echo, echo, echo : " + name;
+	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for CalculateTest task.
+*
+* @author Maxim Ukraintsev
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
